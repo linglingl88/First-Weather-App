3c4944c0b07e8a08a614bdb98bea2281eb870bdd
linglingl88 <108310301+linglingl88@users.noreply.github.com>
2022-07-23 23:45:07 -0700

    added C/F functions

--- src/script.js
+++ src/script.js
@@ -1,33 +1,34 @@
-let now = new Date();
-let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = weekdays[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
-}
-function time() {
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `${day} ${hour}:${minute}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = weekdays[date.getDay()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
 }
 
 let searchForm = document.querySelector("#search-form");
 let currentLocation = document.querySelector("#current-location");
+let celsiusTemp = null;
 
 function displayWeather(response) {
   document.querySelector("h2").innerHTML = response.data.name;
-  document.querySelector("#temperature-number").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celsiusTemp = response.data.main.temp;
+  document.querySelector("#temperature-number").innerHTML =
+    Math.round(celsiusTemp);
   document.querySelector("#weather-condition").innerHTML =
     response.data.weather[0].main;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.main.feels_like
   );
+  document.querySelector("h3").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(city) {
@@ -54,7 +55,25 @@ function runCurrent() {
   navigator.geolocation.getCurrentPosition(showCurrentWeather);
 }
 
-time();
+let temp = document.querySelector("#temperature-number");
+let fahrenheit = document.querySelector("#fahrenheit");
+let celsius = document.querySelector("#celsius");
+function displayFahrenheit(event) {
+  event.preventDefault();
+  temp.innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
+  fahrenheit.classList.add("active");
+  celsius.classList.remove("active");
+}
+function displayCelsius(event) {
+  event.preventDefault();
+  temp.innerHTML = Math.round(celsiusTemp);
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+}
+
+formatDate();
 searchCity("Oakland");
 searchForm.addEventListener("submit", handleSubmit);
 currentLocation.addEventListener("click", runCurrent);
+fahrenheit.addEventListener("click", displayFahrenheit);
+celsius.addEventListener("click", displayCelsius);
